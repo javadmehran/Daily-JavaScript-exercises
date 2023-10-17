c63899efd6837ec6e5700e214eef2aa323d87158
javad <j.mehran1373@gmail.com>
2023-10-17 16:01:50 +0330

    Array

---
+++ Array/script.js
@@ -0,0 +1,15 @@
+let Name='iman';
+let Family='jonas'
+document.write(Name+' '+Family);
+let Names=['Ali','jim','mohammad','sara']
+let Names2=Names.sort();
+document.write("<br/>");
+document.write(Names.length);
+document.write("<br/>");
+document.write(Names[1]);
+
+
+
+let myArray=new Array('iman','Ali',20)
+myArray[2]='mina';
+document.write(myArray[2])

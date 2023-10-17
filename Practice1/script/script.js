a6b67fb15ef3b51ba1917800ea712796c5abb375
javad <j.mehran1373@gmail.com>
2023-10-17 18:25:02 +0330

    Practice1

---
+++ Practice1/script/script.js
@@ -0,0 +1,12 @@
+console.log(Number('23.2'));
+console.log(+'20');
+console.log(parseInt('25.66'));
+console.log(parseFloat('45'));
+console.log(isNaN(20));
+console.log(isNaN('43'));
+console.log(isNaN('df'));
+console.log(Number.isInteger('6'));
+console.log(Math.sqrt(100));
+console.log(8**(1/3));
+console.log(Math.min(45,23,5658,85));
+console.log(Math.max(837,54,76,34));
